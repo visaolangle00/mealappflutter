c971854b001d14a61a788cc4e7a32e3b8edd93b5
visaolangle00 <visaolangle00@gmail.com>
2021-08-02 20:13:01 +0700

    part2_3

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -11,7 +11,9 @@ class  CategoriesScreen extends StatelessWidget {
         title: const Text('DeliMeal'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES.map((catData) => CategoryItem(
+          catData.id,
           catData.title,
           catData.color,
         ),).toList(),
