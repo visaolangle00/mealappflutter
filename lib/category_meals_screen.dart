41862d311e375c16d3187b32955d2cda1f24cb84
visaolangle00 <visaolangle00@gmail.com>
2021-08-03 12:08:10 +0700

    part 5

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,5 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:mealapp/dummy_data.dart';
+import 'package:mealapp/meal_item.dart';
+
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category meals';
@@ -15,13 +18,25 @@ class CategoryMealsScreen extends StatelessWidget {
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
 
+    final categoryMeals = DUMMY_MEALS.where((meal){
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text('The Recipes For The Category!'),
+      body: ListView.builder(
+        itemBuilder: (ctx, index){
+          return MealItem(
+            title:categoryMeals[index].title,
+            imageUrl:categoryMeals[index].imageUrl,
+            duration:categoryMeals[index].duration,
+            affordability: categoryMeals[index].affordability,
+            complexity:categoryMeals[index].complexity,
+          );
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
